63037d3b5161e619b040f5ee44d60a3ae84e713f
Dammy Ola <dammyolapaix@gmail.com>
2023-09-21 10:34:24 +0000

    Continued useState with authentication example

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,57 @@
 import React, { useState } from "react";
 import FirstComponent from "./components/FirstComponent";
 import SecondComponent from "./components/SecondComponent";
-import { userData } from "./data/user";
+
+const userData = {
+  id: 1,
+  name: "Henri Miensan",
+  email: "henri@gmail.com",
+  age: 20,
+  occupation: "Farmer",
+  role: "ADJUTANT", // The role should either be Adjutant or RSM
+  fuelState: "250 litres",
+  troopsMovement: "300 pers",
+};
 
 function App() {
-  const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+  // const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
-  const [userRole, setUserRole] = useState(null);
+  // const [userRole, setUserRole] = useState(null);
 
-  // Function to log in the user
-  const userLoggIn = (role) => {
-    // set user information and role based on the role
-    if (role === "RSM") {
-      setUserInfo(null); // clear user info
-      setUserRole("RSM");
-    } else if (role === "ADJUTANT") {
-      setUserInfo(userInfo); // set user info (assuming userInfo is an object)
-      setUserRole("ADJUTANT");
-    }
-    setUserIsLoggedIn(true);
+  // // Function to log in the user
+  // const userLoggIn = (role) => {
+  //   // set user information and role based on the role
+  //   if (role === "RSM") {
+  //     setUserInfo(null); // clear user info
+  //     setUserRole("RSM");
+  //   } else if (role === "ADJUTANT") {
+  //     setUserInfo(userInfo); // set user info (assuming userInfo is an object)
+  //     setUserRole("ADJUTANT");
+  //   }
+  //   setUserIsLoggedIn(true);
+  // };
+
+  const userLoggIn = () => {
+    setUserInfo(userData); //    userInfo = userData
+    // setUserIsLoggedIn(true);
+    // console.log(userInfo);
   };
 
   // Function to log out the user
   const logOutUser = () => {
     setUserInfo(null);
-    setUserRole(null);
-    setUserIsLoggedIn(false);
+    // setUserRole(null);
+    // setUserIsLoggedIn(false);
   };
 
   return (
     <div>
-      {userIsLoggedIn && userRole === "RSM" ? (
+      {userInfo !== null && userInfo.role === "RSM" ? (
         <div>
           <p>Sorry, you can't access this page.</p>
           <button onClick={logOutUser}>Log out</button>
         </div>
-      ) : userIsLoggedIn && userRole === "ADJUTANT" ? (
+      ) : userInfo !== null && userInfo.role === "ADJUTANT" ? (
         <div>
           <p>Welcome Sir</p>
           <p>id: {userData.id}</p>
@@ -45,13 +61,14 @@ function App() {
           <p>occupation: {userData.occupation}</p>
           <button onClick={logOutUser}>Log out</button>
         </div>
+      ) : userInfo !== null &&
+        userInfo.role !== "ADJUTANT" &&
+        userInfo.role !== "RSM" ? (
+        <div>You are logged in, but not and RSM or an ADJUTANT</div>
       ) : (
         <div>
           <h1>You are not logged in, please log in</h1>
-          <button onClick={() => userLoggIn("RSM")}>Log in as RSM</button>
-          <button onClick={() => userLoggIn("ADJUTANT")}>
-            Log in as Adjutant
-          </button>
+          <button onClick={userLoggIn}>Log in</button>
         </div>
       )}
       <FirstComponent />
